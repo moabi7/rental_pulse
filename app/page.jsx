03cc0fccd85d1cf928ba0452789e023d495c6580
Lesego <lesego@mydough.co.za>
2024-03-17 14:16:46 +0200

    Add properties dot json

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Link from 'next/link';
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Welcome to Home Page</h1>
-      <Link href="/properties">Go to properties</Link>
-    </div>
+      <>
+        <Hero/>
+        <InfoBoxes/>
+      </>
   )
 };
 
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import Link from 'next/link';
-import Hero from '@/components/Hero';
-import InfoBoxes from '@/components/InfoBoxes';
+import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
 
 const PropertiesPage = () => {
   return (
-    <>
-      <Hero/>
-      <InfoBoxes/>
-    </>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">All Properties</h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6">
+        {
+          properties.length === 0 ? 
+          (<p>No properties found</p>) :
+          properties.map((property) => (
+            <PropertyCard name={property.name}/>
+          ))
+        }
+      </div>
+      </div>
+    </section>
   )
 };
 
